37897ace2da2357dc6908e74ad02ebd436634904
MrScX <shovonx@protonmail.com>
2019-04-08 02:55:40 +0600

    almost done profile tabs

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,46 @@
 // tabs init
-$(document).ready(function(){
+$(document).ready(function () {
     $('.tabs').tabs();
 });
 
 // select field init
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').formSelect();
+});
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
 });
 
 // pp listen for change
 
+const defaultImgLink = "https://www.torrentbd.com/torrent/images/default_avatar.png";
+
 const checkImageValidity = (url, callback) => {
     const img = new Image();
-    img.onload = function() { callback(true); };
-    img.onerror = function() { callback(false); };
+    img.onload = () => callback(true);
+    img.onerror = () => callback(false);
     img.src = url;
+
+    img = null; // cleanup memory
 }
 
-document.getElementById("pp_img_field").addEventListener("input",function(evt){
+document.getElementById("pp_img_field").addEventListener("input", function (evt) {
 
     let imgUrl = this.value;
 
-    checkImageValidity(imgUrl, (exists)=>{
+    checkImageValidity(imgUrl, (exists) => {
 
         const targetImg = document.getElementById("pp_img");
 
-        if(exists){
+        if (exists) {
             targetImg.src = imgUrl;
-        }else{
-            targetImg.src = "https://www.torrentbd.com/torrent/images/default_avatar.png";
+            this.style.borderColor = "green";
+            this.style.boxShadow = "0 0 0 0.2rem rgba(88,214,141,.5)";
+        } else {
+            targetImg.src = defaultImgLink;
+            this.style.borderColor = "crimson";
+            this.style.boxShadow = "0 0 0 0.2rem rgba(220,20,60,.5)";
         }
     });
 });
