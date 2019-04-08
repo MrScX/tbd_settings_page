959bb9a6066e43557e1cfa0ebe4b5f15842d88f4
mrscx <shovonx@protonmail.com>
2019-04-08 21:17:35 +0600

    refactored

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,6 @@
-// tabs init
+
 $(document).ready(function () {
     $('.tabs').tabs();
-});
-
-// select field init
-$(document).ready(function () {
-    $('select').formSelect();
-});
-
-$(document).ready(function(){
     $('.materialboxed').materialbox();
 });
 
@@ -17,6 +9,7 @@ $(document).ready(function(){
 const defaultImgLink = "https://www.torrentbd.com/torrent/images/default_avatar.png";
 
 const checkImageValidity = (url, callback) => {
+
     const img = new Image();
     img.onload = () => callback(true);
     img.onerror = () => callback(false);
@@ -34,13 +27,17 @@ document.getElementById("pp_img_field").addEventListener("input", function (evt)
         const targetImg = document.getElementById("pp_img");
 
         if (exists) {
+
             targetImg.src = imgUrl;
             this.style.borderColor = "green";
             this.style.boxShadow = "0 0 0 0.2rem rgba(88,214,141,.5)";
+
         } else {
+
             targetImg.src = defaultImgLink;
             this.style.borderColor = "crimson";
             this.style.boxShadow = "0 0 0 0.2rem rgba(220,20,60,.5)";
+            
         }
     });
 });
